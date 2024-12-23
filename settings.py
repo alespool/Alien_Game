8133f07e5adac90bad42884497392e8924950fcc
alespool <alessionespoli.97@gmail.com>
2024-12-23 21:55:34 +0100

    Ship and settings classes

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """A class to store all the settings of the game characters"""
+
+    def __init__(self):
+        """Initialize the game's settings"""
+        
+        # Screen settings
+        self.screen_width = 1920
+        self.screen_height = 1080
+        self.bg_color = (65,105,225)
