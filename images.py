4041f454d2ee5206d9ff77200856672caf6c43cf
alespool <alessionespoli.97@gmail.com>
2025-01-06 16:35:04 +0100

    Modify dicitonary to contain more images

--- images.py
+++ images.py
@@ -5,13 +5,15 @@ IMAGES = {
     'UPGRADES': {'shooting_speed': 'images/upgrades/increase_shooting_speed.png',
                 'ship_speed': 'images/upgrades/increase_ship_speed.png',
                 'ship_shield': 'images/upgrades/increase_ship_shield.png',
-                'missile' : 'images/upgrades/bigger_missile.png'},
+                'missile' : 'images/upgrades/missile.png',
+                'laser': 'images/upgrades/laser.png',},
     'ALIENS': {'first_alien': 'images/first_alien_cut.png',
                 'second_alien': 'images/second_alien_cut.png',
                 'third_alien': 'images/third_alien_cut.png'},
     'SHIPS': {'human_ship': 'images/ships_human.png'},
     'BULLETS': {'bullet': 'images/bullets/bullet.png',
-                'missile': 'images/bullets/missile.png'},
+                'missile': 'images/bullets/missile.png',
+                'laser' : 'images/bullets/laser.png'},
     'BACKGROUNDS': {'first_background': 'images/first_background_resize.jpg'}, 
 }
 
