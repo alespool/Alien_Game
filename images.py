6555954a70c2f18fec06ae8bc8c24a80c2dcec96
alespool <alessionespoli.97@gmail.com>
2025-01-06 12:27:12 +0100

    Add images for upgrades and bullets

--- images.py
+++ images.py
@@ -2,14 +2,16 @@ import pygame
 
 
 IMAGES = { 
-    'UPGRADES': {'shooting_speed': 'images/upgrades/increase_shooting_speed.png'},
-                # 'shield': 'images/upgrades/shield_upgrade.png',
-                # 'extra_life': 'images/upgrades/upgrades/extra_life.png'},
+    'UPGRADES': {'shooting_speed': 'images/upgrades/increase_shooting_speed.png',
+                'ship_speed': 'images/upgrades/increase_ship_speed.png',
+                'ship_shield': 'images/upgrades/increase_ship_shield.png',
+                'missile' : 'images/upgrades/bigger_missile.png'},
     'ALIENS': {'first_alien': 'images/first_alien_cut.png',
                 'second_alien': 'images/second_alien_cut.png',
                 'third_alien': 'images/third_alien_cut.png'},
     'SHIPS': {'human_ship': 'images/ships_human.png'},
-    # 'BULLETS': {'bullet': 'images/bullet.png'},
+    'BULLETS': {'bullet': 'images/bullets/bullet.png',
+                'missile': 'images/bullets/missile.png'},
     'BACKGROUNDS': {'first_background': 'images/first_background_resize.jpg'}, 
 }
 
@@ -21,7 +23,7 @@ class Images:
         self.upgrades = self.load_images(IMAGES['UPGRADES'])
         self.aliens = self.load_images(IMAGES['ALIENS'])
         self.ships = self.load_images(IMAGES['SHIPS'])
-        # self.bullets = self.load_images(IMAGES['BULLETS'])
+        self.bullets = self.load_images(IMAGES['BULLETS'])
         self.backgrounds = self.load_images(IMAGES['BACKGROUNDS'])
 
     def load_images(self, image_dict):
